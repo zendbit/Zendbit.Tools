2041529a639fecc330a5a29204353556b5ee2672
amru rosyada <amru.rosyada@gmail.com>
2019-08-24 11:51:33 +0700

    add testcase, AES, MD5

---
+++ TestCase/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Zendbit.Tools.Encryption;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TestCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AES Test Test...");
+            Console.WriteLine("=================");
+            var originalAes = "Original AES Text...";
+            var aesKey = "S3cr3tK3y";
+            var aesEncoded = AESCrypt.New().Encode(
+                originalAes,
+                aesKey
+            );
+
+            var aesDecoded = AESCrypt.New().Decode(
+                aesEncoded,
+                aesKey
+            );
+
+            var aesVerified = AESCrypt.New().Verify(originalAes, aesEncoded, aesKey);
+
+            Console.WriteLine($"Aes original {originalAes} \n Aes encoded {aesEncoded} \n Aes Decoded {aesDecoded} \n Verified {aesVerified}");
+            Console.WriteLine("=================\n\n");
+
+
+            Console.WriteLine("MD5 Test Test...");
+            Console.WriteLine("=================");
+            var originalMD5 = "Original Md5 Text...";
+            var MD5Encoded = MD5Crypt.New().ComputeHash(
+                originalMD5
+            );
+
+            var MD5Verified = MD5Crypt.New().Verify(originalMD5, MD5Encoded);
+
+            Console.WriteLine($"Md5 original {originalMD5} \n MD5Hash {MD5Encoded} \n verified {MD5Verified}");
+            Console.WriteLine("=================\n\n");
+
+
+            Console.WriteLine("SHA1 Test Test...");
+            Console.WriteLine("=================");
+            var originalSHA1 = "Original SHA1 Text...";
+            var sha1Encoded = SHA1Crypt.New().ComputeHash(
+                originalSHA1
+            );
+
+            var sha1Verified = SHA1Crypt.New().Verify(originalSHA1, sha1Encoded);
+
+            Console.WriteLine($"sha1 original {originalSHA1} \n sha1 hash {sha1Encoded} \n verified {sha1Verified}");
+            Console.WriteLine("=================\n\n");
+
+
+            Console.WriteLine("XOR Test Test...");
+            Console.WriteLine("=================");
+            var originalXor = "Original XOR Text...";
+            var xorKey = "S3cr3tK3y";
+            var encodedXor = XORCrypt.New().Encode(
+                originalXor,
+                xorKey
+            );
+
+            var xorVerified = XORCrypt.New().Verify(originalXor, encodedXor, xorKey);
+
+            var xorDecoded = XORCrypt.New().Decode(
+                encodedXor,
+                xorKey
+            );
+
+            Console.WriteLine($"Xor original {originalXor} \n Xor encoded {encodedXor} \n Xor Decoded {xorDecoded} \n Verified {xorVerified}");
+            Console.WriteLine("=================\n\n");
+
+
+            Console.WriteLine("Base64 Test Test...");
+            Console.WriteLine("=================");
+            var originalBase64 = "Original Base64 Text...";
+            var base64Encoded = Base64Crypt.New().Encode(
+                originalBase64);
+
+            var base64Decoded = Base64Crypt.New().Decode(
+                base64Encoded);
+
+            var base64Verified = Base64Crypt.New().Verify(originalBase64, base64Encoded);
+
+            Console.WriteLine($"base64 original {originalBase64} \n base64 encoded {base64Encoded} \n base64 decoded {base64Decoded} \n verified {base64Verified}");
+            Console.WriteLine("=================\n\n");
+        }
+    }
+}
--- Zendbit.Tools/Encryption.cs
+++ Zendbit.Tools/Encryption.cs
@@ -1,32 +1,194 @@
 using System.Text;
 using System.Security.Cryptography;
 using System;
+using System.IO;
 
 namespace Zendbit.Tools.Encryption
 {
+    public class AESCrypt
+    {
+        public static AESCrypt New() => new AESCrypt();
+
+        private readonly Aes aesAlg = Aes.Create();
+
+        private string FixKey(string key)
+        {
+            var keyByte = Encoding.UTF8.GetBytes(key);
+            if (keyByte.Length < 16)
+            {
+                return AddKeySpan(key, 16 - keyByte.Length);
+            }
+            else if (keyByte.Length < 24)
+            {
+                return AddKeySpan(key, 24 - keyByte.Length);
+            }
+            else if (keyByte.Length < 32)
+            {
+                return AddKeySpan(key, 32 - keyByte.Length);
+            }
+
+            return key.Substring(0, 32);
+        }
+
+        private string AddKeySpan(string key, int span)
+        {
+            var sb = new StringBuilder(key);
+            for (var i = 0; i < span; i++)
+            {
+                sb.Append("=");
+            }
+            return sb.ToString();
+        }
+
+        public string Encode(string input, string key)
+        {
+            aesAlg.Key = Encoding.UTF8.GetBytes(FixKey(key));
+            var iv = new byte[16];
+            Array.Copy(aesAlg.Key, iv, iv.Length);
+            aesAlg.IV = iv;
+
+            string encrypted = null;
+
+            // Create an encryptor to perform the stream transform.
+            ICryptoTransform encryptor =
+                aesAlg.CreateEncryptor(
+                    aesAlg.Key,
+                    aesAlg.IV
+                );
+
+            // Create the streams used for encryption.
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(input);
+                    }
+                    encrypted = Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+
+            return encrypted;
+        }
+
+        public string Decode(string encrypted, string key)
+        {
+            aesAlg.Key = Encoding.UTF8.GetBytes(FixKey(key));
+            var iv = new byte[16];
+            Array.Copy(aesAlg.Key, iv, iv.Length);
+            aesAlg.IV = iv;
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            // Create a decryptor to perform the stream transform.
+            ICryptoTransform decryptor =
+                aesAlg.CreateDecryptor(
+                    aesAlg.Key,
+                    aesAlg.IV
+                );
+
+            // Create the streams used for decryption.
+            using (
+                MemoryStream msDecrypt =
+                    new MemoryStream(Convert.FromBase64String(encrypted))
+            )
+            {
+                using (
+                    CryptoStream csDecrypt =
+                        new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read)
+                )
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+
+                        // Read the decrypted bytes from the decrypting stream
+                        // and place them in a string.
+                        plaintext = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+
+        public bool Verify(
+            string input, string encrypted, string key
+        )
+        {
+            return input.Equals(
+                    Decode(encrypted, key)
+                );
+        }
+    }
+
+    public class MD5Crypt
+    {
+        public static MD5Crypt New() => new MD5Crypt();
+
+        private readonly MD5 md5Hash = MD5.Create();
+
+        public string ComputeHash(string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString().ToLower();
+        }
+
+        public bool Verify(string input, string hashed)
+        {
+            return hashed.ToLower()
+                .Equals(ComputeHash(input).ToLower());
+        }
+    }
+
     public class SHA1Crypt
     {
         private readonly SHA1 sha = new SHA1CryptoServiceProvider();
 
-        public static SHA1Crypt New()
+        public static SHA1Crypt New() => new SHA1Crypt();
+
+        public byte[] ComputeHash(byte[] input)
         {
-            return new SHA1Crypt();
+            return sha.ComputeHash(input);
         }
 
-        public byte[] ComputeHash(byte[] data)
-        {
-            return sha.ComputeHash(data);
-        }
-
-        public string ComputeHash(string data)
+        public string ComputeHash(string input)
         {
             return BitConverter
                 .ToString(
-                ComputeHash(
-                    Encoding.UTF8.GetBytes(data)
+                    ComputeHash(
+                        Encoding.UTF8.GetBytes(input)
                     )
                 )
-                .Replace("-", "");
+                .Replace("-", "")
+                .ToLower();
+        }
+
+        public bool Verify(
+            string input, string hashed
+        )
+        {
+            return hashed.ToLower()
+                .Equals(ComputeHash(input));
         }
     }
 
@@ -47,22 +209,26 @@ namespace Zendbit.Tools.Encryption
             return new string(output);
         }
 
-        public string Encode(string data, string key)
+        public string Encode(string input, string key)
         {
-            return Cipher(
-                Base64Crypt.New().Encode(
-                    data
-                ),
-                key
-            );
+            var encrypted = Cipher(input, key);
+            return Base64Crypt.New().Encode(encrypted);
         }
 
-        public string Decode(string cipher, string key)
+        public string Decode(string encrypted, string key)
         {
-            return Base64Crypt.New().Decode(
-                Cipher(
-                    cipher, key
-                )
+            var decode = Base64Crypt.New().Decode(encrypted);
+            return Cipher(decode, key);
+        }
+
+        public bool Verify(
+            string input,
+            string encrypted,
+            string key
+        )
+        {
+            return input.Equals(
+                Decode(encrypted, key)
             );
         }
     }
@@ -81,6 +247,11 @@ namespace Zendbit.Tools.Encryption
         {
             var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
             return Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+        public bool Verify(string input, string encoded)
+        {
+            return input.Equals(Decode(encoded));
         }
     }
 }
